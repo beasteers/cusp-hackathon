bda4de071a0c77ad61062aaf6315c4184d04832d
Ben Steers <ben@bensteers.me>
2017-12-02 12:32:26 -0500

    fixed backwards ascii filtering

--- nlp.py
+++ nlp.py
@@ -77,7 +77,7 @@ def filter_tokens(tokens,
     if remove_emojis:
         tokens = [token for token in tokens if not patterns.emoticon.search(token)]
     if remove_nonascii:
-        tokens = [token for token in tokens if len(tokens) == 1 and ord(tokens[0]) > 128]
+        tokens = [token for token in tokens if len(tokens) == 1 and ord(tokens[0]) < 128]
     if lowercase:
         tokens = [token if remove_emojis or patterns.emoticon.search(token) else token.lower() for token in tokens]
     if remove_stopwords:
